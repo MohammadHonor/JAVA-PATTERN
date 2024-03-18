3f9e016d7bf7d4981c3e1f7f19228ac848a20e3f
md.sharufali987@gmail.com <md.sharufali987@gmail.com>
2024-03-18 18:32:08 +0530

    left star

---
+++ p1.java
@@ -0,0 +1,12 @@
+public class p1 {
+
+    public static void main(String[] args) {
+        //right half star
+       for( int i = 1 ; i<=6; i++) {
+           for( int j= 1 ; j<=i ; j++){
+            System.out.print("*");
+           }
+           System.out.println();
+       }
+    }
+}
