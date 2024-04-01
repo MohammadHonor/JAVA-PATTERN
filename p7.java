b50714338117cb964a7ea98145d963df9298cf60
md.sharufali987@gmail.com <md.sharufali987@gmail.com>
2024-04-01 21:42:43 +0530

    solid rectangle

---
+++ p7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class p7 {
+    public static void main(String[] args) {
+        // solid rectangle 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row ");
+        int row= sc.nextInt();
+        System.out.println("Enter col ");
+        int col= sc.nextInt();
+
+        for(int i=1 ;i<=row; i++){
+
+            for( int j= 1 ; j<=col ; j++){
+
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
